7e87f5e791fc85c115fa60bb798785be34a8817d
Enngage <ennspassion@gmail.com>
2020-06-04 12:09:33 +0200

    uses training content type to load courses

--- src/app/pages/courses.component.ts
+++ src/app/pages/courses.component.ts
@@ -3,6 +3,7 @@ import { map } from 'rxjs/operators';
 
 import { BaseComponent, BaseDependencies } from '../base/base.component';
 import { CourseNew } from '../data/course_new';
+import { Training } from '../data/training';
 
 @Component({
     templateUrl: './courses.component.html',
@@ -30,13 +31,12 @@ export class CoursesComponent extends BaseComponent implements OnInit {
     private loadCourses(language: string): void {
         super.subscribeToObservable(
             this.deliveryClient
-                .items<CourseNew>()
-                .type('course_new')
+                .item<Training>('technical_qualification')
                 .languageParameter(language)
                 .toObservable()
                 .pipe(
                     map((response) => {
-                        this.courses = response.items.filter(m => m.system.language === language);
+                        this.courses = response.item.courses.value.filter(m => m.system.language === language);
                         super.markForCheck();
                     })
                 )
